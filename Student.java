545a57e4a994f2cc7962de96f66d0b6b7b0f47f7
nish <144939153+nishj347@users.noreply.github.com>
2025-02-11 11:28:14 +0530

    Student.java

--- Student.java
+++ Student.java
@@ -56,3 +56,17 @@ public class Main {
         student2.display();
     }
 }
+
+
+// output
+"C:\Program Files\Java\jdk-23\bin\java.exe" "-javaagent:C:\Users\nishj\OneDrive\Desktop\IntelliJ IDEA Community Edition 2024.3.1.1\lib\idea_rt.jar=55536:C:\Users\nishj\OneDrive\Desktop\IntelliJ IDEA Community Edition 2024.3.1.1\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath "C:\Users\nishj\Real time ticket booking\out\production\Real time ticket booking" Main
+Student Name: Nish
+Roll Number: 101
+Marks: 92.0
+Grade: A
+Student Name: Ayush
+Roll Number: 102
+Marks: 75.0
+Grade: C
+
+Process finished with exit code 0
